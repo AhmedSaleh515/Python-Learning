ac3b6f07caf57f4cc453a187ba4ec2d85f66a860
AhmedSaleh515 <hamada.alhebshi@gmail.com>
2026-03-15 22:48:44 +0800

    Add my first python script

---
+++ python.py
@@ -0,0 +1,14 @@
+first_number = int (print("enter the first number: "))
+operator = print("enter an operator")
+second_number = int(print("enter the second number")) 
+
+if operator == '+':
+    print((first_number + second_number))
+elif operator == '-':
+    print((first_number - second_number))
+elif operator == '*':
+    print((first_number * second_number))
+elif operator == '/':
+    print((first_number / second_number))        
+elif operator == '%':
+    print((first_number % second_number))
