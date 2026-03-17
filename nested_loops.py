a35692fde7420ec65a6ea7db0c065eb81f0afb45
AhmedSaleh515 <hamada.alhebshi@gmail.com>
2026-03-17 19:56:30 +0800

    another example in nested loops

--- nested_loops.py
+++ nested_loops.py
@@ -5,4 +5,11 @@ for left in range(7):
         print("[" + str(left) + "|" + str(right) + "]", end=" ")
     print()
 
-    
+# Now i will try to print time table for football league. The league has 4 teams and each team will play against each other team twice (home and away).    
+print("______________________________________")
+print("Football League Schedule:")
+teams = ['Barcelona', 'Real Madrid', 'Manchester City', 'Arsenal']
+for home in teams:
+    for away in teams:
+        if home != away:
+            print(home + " vs " + away)
