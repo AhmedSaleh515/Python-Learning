f54624f2dde9d311a8f48da8f08ccb325e90f76f
AhmedSaleh515 <hamada.alhebshi@gmail.com>
2026-03-18 15:31:38 +0800

    slice examples

---
+++ slice.py
@@ -0,0 +1,10 @@
+# this code show you all possible ways i know, to use slices
+string1 = "Greeting, Earthligs"
+print(string1[0])
+print(string1[4:8])
+print(string1[10:])
+print(string1[:5])
+print(string1[-9:])
+print(string1[55:])
+print(string1[0::2])
+print(string1[::-1])
