66aeb32a2e13f7a4224346a09155f8507c84dba9
AhmedSaleh515 <hamada.alhebshi@gmail.com>
2026-03-21 18:22:24 +0800

    string practice

---
+++ strings.py
@@ -0,0 +1,11 @@
+name = "HAMADA" 
+print(name[1])
+print(name[0])
+print(name[5])
+#print(name[6])  out of range
+
+#if i want the last char but i don't know how long is the string is 
+#then i should use '-' negative sign
+#its let me move backward
+print(name[-1])
+print(name[-2])
