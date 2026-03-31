903c3a995bbfcd99e82db7b4eb19482f8cac8210
AhmedSaleh515 <hamada.alhebshi@gmail.com>
2026-03-31 23:46:19 +0800

    intro to lists

---
+++ lists.py
@@ -0,0 +1,11 @@
+x = ['now', 'we', 'are', 'cooking']
+print(type(x))
+print(x)
+print(len(x))
+print(('are' in x), "|", ('today' in x))
+print(x[0])
+print(x[3])
+#print(x[4])      #This last line will throw an error cos its out of range
+print(x[1:3])
+print(x[:2])
+print(x[2:])
