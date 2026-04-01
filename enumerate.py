a0b361c1c58c30563ade932f68ad8e3d4f1acb45
AhmedSaleh515 <hamada.alhebshi@gmail.com>
2026-04-01 15:31:18 +0800

    simple example for enumerate

---
+++ enumerate.py
@@ -0,0 +1,4 @@
+# example for enumerate method
+names = ('Ahmed', 'Mohammed', 'Hamid', 'Abdulrahman', 'Fatima', 'Abdullah')
+for index, name in enumerate(names):
+    print("{}- {}".format(index+1,name))
